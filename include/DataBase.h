75d62882ac477391430e939342058aff64af49ec
4evrinspace <dengavlov@gmail.com>
2024-11-26 20:23:35 +0300

    Added small of funcparser

--- include/DataBase.h
+++ include/DataBase.h
@@ -27,6 +27,8 @@ class DataBase
     Table deleteRows(std::string s, Condition &cond);
     Table update(std::string s, std::string allexpr, std::string cond);
 
+    Table exucute(std::string);
+
   public:
     std::map<std::string, Table> tables;
     Table query(std::string q);
---
+++ src/funcparser.cpp
@@ -0,0 +1,135 @@
+#include <DataBase.h>
+#include <algorithm>
+
+static std::string strip(std::string str)
+{
+    int left = 0;
+    int right = str.size() - 1;
+    while (std::isspace(static_cast<unsigned char>(str[left])))
+        left++;
+    while (std::isspace(static_cast<unsigned char>(str[right])))
+        right--;
+    return str.substr(left, right - left + 1);
+}
+static std::string get_space_word(std::string str, int &start)
+{
+    int left = start;
+    bool is_opened = false;
+    for (; start < str.size(); start++)
+    {
+        if (!std::isspace(static_cast<unsigned char>(str[start])) && !is_opened)
+        {
+            is_opened = true;
+            left = start;
+        }
+        else if (std::isspace(static_cast<unsigned char>(str[start])) && is_opened)
+        {
+
+            return str.substr(left, start - left);
+        }
+    }
+    return "";
+}
+
+static int unquote_find(std::string origin_str, std::string to_find)
+{
+    int first = 0;
+    int second = origin_str.find(to_find);
+
+    for (; first != std::string::npos;
+         first = origin_str.find(to_find, second + 1), second = origin_str.find(to_find, first + 1))
+    {
+        int pos = origin_str.find(to_find, first);
+        if (pos == std::string::npos)
+            return -1;
+        if (pos < second)
+        {
+            return pos;
+        }
+        int moving_ptr = second;
+        do
+        {
+            first = origin_str.find(to_find, moving_ptr + 1);
+            moving_ptr = first;
+        } while (first != std::string::npos && origin_str[first - 1] == '//');
+        moving_ptr = first;
+
+        do
+        {
+            second = origin_str.find(to_find, moving_ptr + 1);
+            moving_ptr = first;
+        } while (first != std::string::npos && origin_str[first - 1] == '//');
+    }
+    return -1;
+}
+Table DataBase::exucute(std::string query)
+{
+    int pos = 0;
+    std::string type_query = get_space_word(query, pos);
+    std::transform(type_query.begin(), type_query.end(), type_query.begin(),
+                   [](unsigned char c) { return std::tolower(c); });
+    query = query.substr(pos);
+    if (type_query == "create")
+    {
+        /*
+        auto res = CreateQuery::parse(query.substr(pos));
+        return createTable(res.first, res.second);
+        */
+    }
+    else if (type_query == "insert")
+    {
+        /*
+        auto res = InsertQuery::parse(query.substr(pos));
+        return insert(res.first, res.second);
+        */
+    }
+    else if (type_query == "delete")
+    {
+        int where_pos = unquote_find(query, std::string("where"));
+        std::string from_table;
+        Condition tmp(std::string(""));
+        if (where_pos == -1)
+            return deleteRows(strip(query), tmp);
+        from_table = strip(query.substr(0, where_pos));
+        tmp = Condition(query.substr(where_pos + 5));
+        return deleteRows(from_table, tmp);
+    }
+    else if (type_query == "update")
+    {
+        int set_pos = unquote_find(query, std::string("set"));
+        int where_pos = unquote_find(query, std::string("where"));
+        if (set_pos == -1)
+            throw std::runtime_error("FuncParser:: no set in update found");
+        if (where_pos == -1)
+            return update(strip(query.substr(0, set_pos)), query.substr(set_pos + 3), "");
+        return update(strip(query.substr(0, set_pos)), query.substr(set_pos + 3, where_pos - set_pos - 2), query.substr(where_pos + 5));
+    }
+    else if (type_query == "select")
+    {
+        int from_pos = unquote_find(query, std::string("from"));
+        int where_pos = unquote_find(query, std::string("where"));
+        std::vector<std::string> columns;
+        Condition tmp(std::string(""));
+        if (from_pos == -1)
+            throw std::runtime_error("FuncParser:: no from in select  found");
+        int prev = 0;
+        int next = query.find(',', prev);
+        while (next != std::string::npos && next < from_pos )
+        {
+            columns.emplace_back(strip(query.substr(prev, next - prev)));
+            prev = next + 1;
+            next = query.find(',', prev);
+        }
+        if (where_pos == -1)
+        {
+            return select(strip(query.substr(from_pos + 4, where_pos - from_pos - 4)), columns, tmp);
+        }
+        tmp = Condition(query.substr(where_pos + 5));
+        return select(strip(query.substr(from_pos + 4, where_pos - from_pos - 4)), columns, tmp);
+    }
+    else
+    {
+
+        throw std::runtime_error("FuncParser:: No matching command was found");
+    }
+}
